c2b74d9c5c943ea0d2beb1f97dcf6c6fed87fd86
Sumit Ojha <sumit.ojha.dev@gmail.com>
2022-07-09 11:25:16 -0500

    created new features for searching the meals by ingredients

--- script.js
+++ script.js
@@ -1,55 +1,102 @@
-//gathering API Credentials
-var appId = "cdc87a4e";
-var appKey = "8a7be31efd6be7e303b00d3e963e0c18";
-var baseURL = `https://api.edamam.com/search?q=pizza&app_id=${appId}&app_key=${appKey}&to=20`;
+const searchButton = document.querySelector("#search-btn");
+const mealList = document.querySelector("#meal");
+const mealDetailsContent = document.querySelector(".meal-details-content");
+const recipeCloseButton = document.querySelector("#recipe-close-btn");
+const searchHistory = document.querySelector(".search-history-list");
 
-//applying Fetch method to access data from the API
-fetch(baseURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    searchRecipe(data);
-  });
+//localStorage.setItem("ingredientSearch");
 
-//reflect the searched food item in the recipe search section
-var recipeSearch = document.getElementById("recipeItems");
-function searchRecipe() {
-  document.getElementById("displayItem").innerHTML = recipeSearch.value;
-  recipeSearch.value = "";
+//handling click on search button
+let ingredientSearch = document.querySelector("#search-input").value.trim();
+
+localStorage.setItem("recipeSearch", ingredientSearch);
+
+if (localStorage.getItem("recipeSearch") != null) {
+  var historyTmp = localStorage.getItem("recipeSearch");
+  var oldhistoryarray = historyTmp.split("|");
+
+  for (var i = 0; i < oldhistoryarray.length; i++) {
+    searchHistory.innerHTML = "<p>" + oldhistoryarray[i] + "</p>";
+  }
 }
 
-//populate and store the history of the grocery list
-var inputField = document.getElementById("inputField");
-var addToDoButton = document.getElementById("addToDo");
-var toDoContainer = document.getElementById("toDoContainer");
+searchButton.addEventListener("click", getListofMeals);
 
-addToDoButton.addEventListener("click", function () {
-  var paragraph = document.createElement("p");
-  paragraph.classList.add("paragraph-styling"); //paragraph-styling is a reference from CSS
-  paragraph.innerText = inputField.value;
-  toDoContainer.appendChild(paragraph);
-  inputField.value = " ";
+mealList.addEventListener("click", getRecipe);
 
-  paragraph.addEventListener("click", function () {
-    paragraph.style.textDecoration = "line-through";
-  });
-  paragraph.addEventListener("dblclick", function () {
-    toDoContainer.removeChild(paragraph);
-  });
-});
+recipeCloseButton.addEventListener("click", () =>
+  mealDetailsContent.parentElement.classList.remove("showRecipe")
+);
 
-//save the history of the grocery list in the local storage
-if (localStorage.getItem("inputField") !== null) {
-  document.getElementById("inputField").innerHTML = JSON.parse(
-    localStorage.getItem("inputField")
-  );
-  console.log(localStorage.getItem("inputField"));
+//function to get the list of meals
+
+function getListofMeals() {
+  fetch(
+    "https://www.themealdb.com/api/json/v1/1/filter.php?i=" + ingredientSearch
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      let addingHtml = "";
+      if (data.meals) {
+        data.meals.forEach((meal) => {
+          addingHtml += ` 
+          <div class="meal-item" data-id = "${meal.idMeal}">
+              <div class="meal-img">
+                <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+              </div>
+              <div class="meal-name">
+                <h3>${meal.strMeal}</h3>
+                <a href="#" class="recipe-btn">Get Recipe Info</a>
+              </div>
+          </div>`;
+        });
+        mealList.classList.remove("foodNotFound");
+      } else {
+        addingHtml += `<h4>Sorry, no meals can be found by such ingredient.</h4>`;
+        mealList.classList.add("foodNotFound");
+      }
+      mealList.innerHTML = addingHtml;
+    });
 }
 
-function addGrocery() {
-  var TODO = document.getElementById("inputField").value;
-  var jsonTODO = JSON.stringify(TODO);
-  localStorage.setItem("inputField", jsonTODO);
+//function to get the recipe of the meal
+
+function getRecipe(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("recipe-btn")) {
+    let mealItem = e.target.parentElement.parentElement;
+    console.log(mealItem.dataset.id);
+    fetch(
+      "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" +
+        mealItem.dataset.id
+    )
+      .then((response) => response.json())
+      .then((data) => getMealRecipe(data.meals));
+  }
+}
+
+//function to display recipe in html
+
+function getMealRecipe(meals) {
+  meals = meals[0];
+  console.log(meals);
+  let addedHtml = `<h2 class="recipe-title">${meals.strMeal}</h2>
+            <p class="recipe-category">${meals.strCategory}</p>
+            <div class="recipe-instruction">
+              <h3>Instructions</h3>
+              <p>
+                ${meals.strInstructions}
+              </p>
+             
+            </div>
+            <div class="recipe-meal-img">
+              <img src="${meals.strMealThumb}" alt="${meals.strMeal}" />
+            </div>
+            <div class="recipe-link">
+              <a href="${meals.strYoutube}" target="_blank">Watch Tutorial</a>
+            </div>`;
+
+  mealDetailsContent.innerHTML = addedHtml;
+  mealDetailsContent.parentElement.classList.add("showRecipe");
 }
