9cd3e57365318a938bdd8970ca1f42bd1aa3fca4
Your Name <you@example.com>
2022-07-09 18:59:20 -0600

    displayed API data into the browser

--- script.js
+++ script.js
@@ -2,29 +2,81 @@
 //gathering API Credentials
 var appId='cdc87a4e';
 var appKey = '8a7be31efd6be7e303b00d3e963e0c18';
-var baseURL = `https://api.edamam.com/search?q=pizza&app_id=${appId}&app_key=${appKey}&to=20`;
+var key = 'pizza'
+var baseURL = `https://api.edamam.com/search?q=${key}&app_id=${appId}&app_key=${appKey}&to=20`;
 
 
 
 
 //applying Fetch method to access data from the API
-fetch (`https://api.edamam.com/search?q=pizza&app_id=${appId}&app_key=${appKey}`)
-.then(function(response){
-return response.json();
+
+async function fetchData(userInput){
+  key = userInput;
+  baseURL = `https://api.edamam.com/search?q=${key}&app_id=${appId}&app_key=${appKey}&to=20`;
+fetch(baseURL)
+.then((response) => {
+  return response.json();
 })
-.then(function(data){
-   console.log(data);
-searchRecipe(data)
-})
+.then((data) => {
+  
+  var generatedHTML = '';
+   
+     data.hits.map(function(result) {
+     generatedHTML+=
+     console.log(result);
+     generatedHTML+=
+  
+                 `<div id = "DISPLAY" class="display" >
+                <img id="picture" src="${result.recipe.image}">
+                <div class="recipeTabs">
+                  <div>${result.recipe.label}"</div>
+                  <div><a id="viewRecipe" href="${result.recipe.url}" target=_"blank"> View Recipe</a></div>
+                  <div>Calories:${result.recipe.calories.toFixed(2)}</div>
+                </div>`;
+                
+   
+   document.getElementById("DISPLAY").innerHTML=  generatedHTML;
+  });
+  
+  });
+
+  //generateHTML (authors);
+}
 
 
 
 
-//reflect the searched food item in the recipe search section
-var recipeSearch = document.getElementById("recipeItems");
+
+
+function generateHTML (data){ 
+   var generatedHTML = '';
+   
+//    data.map(result=> {
+//    //generatedHTML+=
+//    console.log(result);
+//    generatedHTML+=
+
+//                `<div id = "DISPLAY" class="display" >
+//               <img id="picture" src="${result.recipe.image}">
+//               <div class="recipeTabs">
+//                 <div>${result.recipe.label}"</div>
+//                 <div><a id="viewRecipe" href="${result.recipe.url}" target=_"blank"> View Recipe</a></div>
+//                 <div>Calories:${result.recipe.calories.toFixed(2)}</div>
+//               </div>`
+//  });
+ document.getElementById("DISPLAY").innerHTML=  generatedHTML;
+ 
+}
+  
+ 
+
+
 function searchRecipe(){
-document.getElementById("displayItem").innerHTML= recipeSearch.value
-recipeSearch.value = "";
+  //reflect the searched food item in the recipe search section
+var recipeSearch = document.getElementById("recipeItems");
+document.getElementById("displayItem").innerHTML= recipeSearch.value;
+fetchData(recipeSearch.value);
+
 }
 
 
@@ -64,3 +116,5 @@ var TODO = document.getElementById("inputField").value;
 var jsonTODO = JSON.stringify (TODO);
 localStorage.setItem("inputField", jsonTODO);
 }
+
+
