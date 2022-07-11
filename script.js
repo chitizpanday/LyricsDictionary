9f319afc81f770158adc7dc4beaf557d66127029
Your Name <you@example.com>
2022-07-10 19:35:46 -0600

    fetched NewYork Times API credentials

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-//gathering API Credentials
-<<<<<<< HEAD
+//gathering API Credentials for food Search from edamam
 var appId='cdc87a4e';
 var appKey = '8a7be31efd6be7e303b00d3e963e0c18';
 var key = 'pizza'
-var baseURL = `https://api.edamam.com/search?q=${key}&app_id=${appId}&app_key=${appKey}&to=20`;
-=======
-var appId = "cdc87a4e";
-var appKey = "8a7be31efd6be7e303b00d3e963e0c18";
-var baseURL = `https://api.edamam.com/search?q=pizza&app_id=${appId}&app_key=${appKey}&to=20`;
->>>>>>> ae7a5df5a375ae32a6a7ea97d9df017468888321
-
+var baseURL = `https://api.edamam.com/search?q=${key}&app_id=${appId}&app_key=${appKey}&to=3`;
+var displaySearchResults=document.getElementById("displayPictures")
 //applying Fetch method to access data from the API
-<<<<<<< HEAD
 
 async function fetchData(userInput){
   key = userInput;
-  baseURL = `https://api.edamam.com/search?q=${key}&app_id=${appId}&app_key=${appKey}&to=20`;
+  baseURL = `https://api.edamam.com/search?q=${key}&app_id=${appId}&app_key=${appKey}&to=3`;
 fetch(baseURL)
 .then((response) => {
   return response.json();
@@ -26,8 +19,8 @@ fetch(baseURL)
    
      data.hits.map(function(result) {
      generatedHTML+=
-     console.log(result);
-     generatedHTML+=
+    //  console.log(result);
+    //  generatedHTML+=
   
                  `<div id = "DISPLAY" class="display" >
                 <img id="picture" src="${result.recipe.image}">
@@ -36,40 +29,24 @@ fetch(baseURL)
                   <div><a id="viewRecipe" href="${result.recipe.url}" target=_"blank"> View Recipe</a></div>
                   <div>Calories:${result.recipe.calories.toFixed(2)}</div>
                 </div>`;
+
+               
                 
    
-   document.getElementById("DISPLAY").innerHTML=  generatedHTML;
+                displaySearchResults.innerHTML=  generatedHTML;
   });
   
   });
-
-  //generateHTML (authors);
 }
 
+//gathering API Credentials for food Search from Newyork Times
+var applicationId='6f3d81b6-bfad-49ed-b46e-b805cc54da80';
+var applicationKey = 'QYRw2zwbEnHj7w1ydbnka8cYblkU2I10';
+URL = `https://api.nytimes.com/svc/topstories/v2/food.json?api-key=${applicationKey}`
 
 
 
 
-
-function generateHTML (data){ 
-   var generatedHTML = '';
-   
-//    data.map(result=> {
-//    //generatedHTML+=
-//    console.log(result);
-//    generatedHTML+=
-
-//                `<div id = "DISPLAY" class="display" >
-//               <img id="picture" src="${result.recipe.image}">
-//               <div class="recipeTabs">
-//                 <div>${result.recipe.label}"</div>
-//                 <div><a id="viewRecipe" href="${result.recipe.url}" target=_"blank"> View Recipe</a></div>
-//                 <div>Calories:${result.recipe.calories.toFixed(2)}</div>
-//               </div>`
-//  });
- document.getElementById("DISPLAY").innerHTML=  generatedHTML;
- 
-}
   
  
 
@@ -80,22 +57,6 @@ var recipeSearch = document.getElementById("recipeItems");
 document.getElementById("displayItem").innerHTML= recipeSearch.value;
 fetchData(recipeSearch.value);
 
-=======
-fetch(baseURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    searchRecipe(data);
-  });
-
-//reflect the searched food item in the recipe search section
-var recipeSearch = document.getElementById("recipeItems");
-function searchRecipe() {
-  document.getElementById("displayItem").innerHTML = recipeSearch.value;
-  recipeSearch.value = "";
->>>>>>> ae7a5df5a375ae32a6a7ea97d9df017468888321
 }
 
 //populate and store the history of the grocery list
@@ -126,7 +87,6 @@ if (localStorage.getItem("inputField") !== null) {
   console.log(localStorage.getItem("inputField"));
 }
 
-<<<<<<< HEAD
 
 function addGrocery(){
 var TODO = document.getElementById("inputField").value;
@@ -135,10 +95,3 @@ localStorage.setItem("inputField", jsonTODO);
 }
 
 
-=======
-function addGrocery() {
-  var TODO = document.getElementById("inputField").value;
-  var jsonTODO = JSON.stringify(TODO);
-  localStorage.setItem("inputField", jsonTODO);
-}
->>>>>>> ae7a5df5a375ae32a6a7ea97d9df017468888321
